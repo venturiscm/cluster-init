6a65d2062b8d2c4a0cf6106ac91765b7276b49e7
Adrian Webb <adrian@webb.sh>
2018-04-29 20:20:37 -0400

    Adding an initial HypriotOS network setup script.

---
+++ scripts/hypriot/network.sh
@@ -0,0 +1,39 @@
+#/bin/bash
+
+NETWORK_DIR="/etc/network/interfaces.d"
+HOSTNAME="$1"
+STATIC_IP="$2"
+
+# Validation
+if [ -z "$HOSTNAME" ]
+then
+  echo "Hostname is required to set up network"
+  exit 1
+fi
+if [ -z "$STATIC_IP" ]
+then
+  echo "Static IP is required to set up network"
+  exit 2
+fi
+
+GATEWAY_IP="$(sed -r 's/[0-9]+$/1/' <<< $STATIC_IP)"
+
+# Hostname
+echo "$HOSTNAME" > /etc/hostname
+
+# Network cleanup
+#rm -f "$NETWORK_DIR/"*.cfg
+
+# Localhost generation
+echo "
+auto lo
+iface lo inet loopback
+" > "$NETWORK_DIR/localhost"
+
+# eth0 generation
+echo "
+iface eth0 inet static
+address "$STATIC_IP"
+gateway "$GATEWAY_IP"
+netmask 255.255.255.0
+" > "$NETWORK_DIR/eth0"
