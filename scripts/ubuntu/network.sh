37765675c2cef6e6fe800b8a448d703e330078e9
Adrian Webb <adrian@webb.sh>
2018-04-29 21:32:06 -0400

    Adding an initial ubuntu network setup script.

---
+++ scripts/ubuntu/network.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+
+NETWORK_DIR="/etc/netplan"
+HOSTNAME="$1"
+STATIC_IP="$2"
+
+# Validation
+if [ -z "$HOSTNAME" ]
+then
+  echo "Hostname is required to set up network"
+  exit 1
+fi
+if [ -z "$STATIC_IP" ]
+then
+  echo "Static IP is required to set up network"
+  exit 2
+fi
+
+GATEWAY_IP="$(sed -r 's/[0-9]+$/1/' <<< $STATIC_IP)"
+
+# Hostname
+echo "$HOSTNAME" > /etc/hostname
+
+# Network cleanup
+rm -f "$NETWORK_DIR/"*.yaml
+
+# eth0 generation
+echo "
+network:
+ version: 2
+ renderer: networkd
+ ethernets:
+   enp1s0:
+     dhcp4: no
+     dhcp6: no
+     addresses: ["$STATIC_IP/24"]
+     gateway4: "$GATEWAY_IP"
+" > "$NETWORK_DIR/enp1s0.yaml"
